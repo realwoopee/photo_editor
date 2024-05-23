082f5bab7e582ac0489f87dd6a74ba3970a6e4bc
Danila Pokhabov <danilapokhabov@gmail.com>
2024-05-23 16:44:54 +0700

    Import openCV

--- app/src/main/java/com/pokhuimand/photoeditor/MainActivity.kt
+++ app/src/main/java/com/pokhuimand/photoeditor/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.pokhuimand.photoeditor
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.ComposeView
 import androidx.core.view.ViewCompat
 import com.pokhuimand.photoeditor.ui.PhotoEditorNavGraph
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
+
+        if (OpenCVLoader.initLocal()) {
+            Log.i("OpenCV", "OpenCV successfully loaded.");
+        }
 
         val appContainer = (application as PhotoEditorApplication).appContainer;
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Photo Editor"
 include(":app")
+include(":opencv")
